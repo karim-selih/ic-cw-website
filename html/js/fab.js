ab94ac8a10fea46cd8fea00a633fedc5be190fbe
Raihaan Rahman <raihaan2000@gmail.com>
2019-08-06 14:20:31 +0100

    Updated fab so that it dissapears when scrolled into the footer

--- html/js/fab.js
+++ html/js/fab.js
@@ -1,10 +1,23 @@
+function isScrolledIntoView(el) {
+   // Source: https://stackoverflow.com/questions/46651373/disable-materialize-css-modal-footer-button
+   var rect = el.getBoundingClientRect();
+   var elemTop = rect.top;
+   var elemBottom = rect.bottom;
+
+   // Only completely visible elements return true:
+   var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+   // Partially visible elements return true:
+   // isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+   return isVisible;
+}
+
 $("a[href='#top']").click(function () {
    $("html, body").animate({ scrollTop: 0 }, "fast");
    return false;
 });
 
 $(window).scroll(function () {
-   if ($(this).scrollTop() > 300) {
+   if (!isScrolledIntoView(document.getElementsByClassName('page-footer')[0]) && $(this).scrollTop() > 300) {
       $(".scroll-to-top").removeClass("scale-out");
    } else {
       $(".scroll-to-top").addClass("scale-out");
