6e39e5f0cb15c91ceb0be37e6668a0d46c254413
Raihaan Rahman <raihaan2000@gmail.com>
2019-08-15 23:05:49 +0100

    Added a class to deal with the last two events cards

--- html/js/main-page-tablet-optimisations.js
+++ html/js/main-page-tablet-optimisations.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
       $(".event").addClass("l6")
       $(".about").addClass("l6");
       $("#third-about").addClass("push-l3");
+      $(".penultimate-event").removeClass("offset-l2");
+      $(".last-event").removeClass("pull-l2");
    }
    else {
       $(".about").addClass("l4");
@@ -14,11 +16,16 @@ $(window).resize(function () {
       $(".event").addClass("l6");
       $(".about").addClass("l6");
       $("#third-about").addClass("push-l3");
+      $(".penultimate-event").removeClass("offset-l2");
+      $(".last-event").removeClass("pull-l2");
+      
    }
    else {
       $(".event").removeClass("l6");
       $(".about").removeClass("l6");
       $("#third-about").removeClass("push-l3");
       $(".about").addClass("l4");
+      $(".penultimate-event").addClass("offset-l2");
+      $(".last-event").addClass("pull-l2");
    }
 });
